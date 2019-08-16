cf3619498a571411e086b323c835ba0d18b75522
naneri <ktnaneri@gmail.com>
2019-08-16 22:19:22 +0600

    permissions

--- deploy/permissions.sh
+++ deploy/permissions.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# install composer dependencies
+docker run --rm --interactive --tty \
+  --volume $PWD:/app \
+  composer install
+
+# set folder permissions
 sudo chgrp -R www-data storage bootstrap/cache
 sudo chmod -R ug+rwx storage bootstrap/cache
